ab92f2e5f4adb43b6ae0ad03d76e32391bf64093
daniNKV <nikiforovdaniel@gmail.com>
2024-12-07 06:45:16 -0300

    setup authentication and style auth layout with sign in and password recovery views

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,11 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "theme-change" # @2.5.0
 pin "theme", to: "theme.js"
 
+
+pin "@stimulus-components/password-visibility", to: "@stimulus-components--password-visibility.js" # @3.0.0
