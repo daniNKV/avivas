7e8255300009bc7a6db05fb0344c041b1da6af65
daniNKV <nikiforovdaniel@gmail.com>
2024-12-08 01:50:53 -0300

    add missing users attributes and views icons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,68 @@
 class User < ApplicationRecord
   include Clearance::User
+  has_one_attached :avatar
+  before_save :normalize_phone_number
 
-  has_secure_password
+  enum :role, {
+    member: 0,
+    employee: 1,
+    manager: 2,
+    admin: 3
+  }, suffix: true
+
+  enum :status, { active: 0, inactive: 1, blocked: 2 }, suffix: true
+
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: {
+              minimum: 3,
+              maximum: 50
+            },
+            format: {
+              with: /\A[a-zA-Z0-9_]+\z/,
+              message: "can only contain letters, numbers, and underscores"
+            }
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: {
+              with: URI::MailTo::EMAIL_REGEXP,
+              message: "must be a valid email address"
+            }
+
+  validates :phone, phone: true
+
+  validates :first_name,
+            length: {
+              minimum: 2,
+              maximum: 50,
+              allow_blank: true
+            }
+
+  validates :last_name,
+            length: {
+              minimum: 2,
+              maximum: 50,
+              allow_blank: true
+            }
+
+  validates :role,
+            presence: true,
+            inclusion: { in: roles.keys }
+
+  validates :joined_at,
+            presence: true,
+            comparison: { less_than_or_equal_to: Date.today }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def normalize_phone_number
+    self.phone= Phonelib.parse(phone).international if phone.present?
+  end
 end
--- db/migrate/20241207051101_create_users.rb
+++ db/migrate/20241207051101_create_users.rb
@@ -1,10 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :password_digest
-      t.datetime :joined_at
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :phone
+      t.string :first_name, limit: 50
+      t.string :last_name, limit: 50
+      t.integer :role, default: 0, null: false
+      t.integer :status, default: 0, null: false
+      t.date :joined_at
 
       t.timestamps
     end
