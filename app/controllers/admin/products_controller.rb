868cc3bf93da4d3b2f14c78d564b799a76fb7d54
daniNKV <nikiforovdaniel@gmail.com>
2024-12-11 01:17:41 -0300

    fix: correct styles and typos

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -29,7 +29,7 @@ class Admin::ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: "Product was successfully created." }
+        format.html { redirect_to admin_product_path @product, notice: "Product was successfully created." }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class Admin::ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(admin_product_params)
-        format.html { redirect_to @product, notice: "Product was successfully updated." }
+        format.html { redirect_to admin_product_path @product, notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -69,6 +69,12 @@ class Admin::ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def admin_product_params
-      params.fetch(:product, {})
+      params.require(:product).permit(
+        :name,
+        :description,
+        :base_price,
+        :stock_quantity,
+        images: [],
+      )
     end
 end
--- db/migrate/20241210031417_create_products.rb
+++ db/migrate/20241210031417_create_products.rb
@@ -3,11 +3,11 @@ class CreateProducts < ActiveRecord::Migration[8.0]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.decimal :base_price
-      t.integer :stock_quantity
-      t.boolean :published
-      t.boolean :deleted
-      t.datetime :deleted_at
+      t.decimal :base_price, default: 0.0
+      t.integer :stock_quantity, default: 0
+      t.boolean :published, default: true
+      t.boolean :deleted, default: false
+      t.datetime :deleted_at, default: nil
 
       t.timestamps
     end
